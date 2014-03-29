0982f6aeaae974c0cb56b017f6c47e24fba0cce0
kbeal <kyledbeal@gmail.com>
2014-03-29 12:16:39 -0400

    Added ViewPickerViewController

---
+++ JenkinsMobile/KDBViewPickerViewController.h
@@ -0,0 +1,21 @@
+//
+//  KDBViewPickerViewController.h
+//  JenkinsMobile
+//
+//  Created by Kyle on 3/29/14.
+//  Copyright (c) 2014 Kyle Beal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol ViewPickerDelegate <NSObject>
+@required
+-(void)selectedView:(NSString *)newViewURL;
+@end
+
+@interface KDBViewPickerViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *views;
+@property (nonatomic, weak) id<ViewPickerDelegate> delegate;
+
+@end
