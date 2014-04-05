b793238bec3088fa1252e64fc4ccbdaa24e5ecc7
kbeal <kyledbeal@gmail.com>
2014-04-04 20:14:47 -0400

    passing test of inserting jobs

--- JenkinsMobileTests/JenkinsMobileTests.m
+++ JenkinsMobileTests/JenkinsMobileTests.m
@@ -8,8 +8,10 @@
 
 #import <XCTest/XCTest.h>
 #import "KDBJenkinsRequestHandler.h"
+#import "Job.h"
 
 @interface JenkinsMobileTests : XCTestCase
+@property (nonatomic, strong) NSManagedObjectContext *context;
 
 @end
 
@@ -18,21 +20,54 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    NSManagedObjectModel *model = [NSManagedObjectModel mergedModelFromBundles: nil];
+    NSLog(@"model: %@", model);
+    NSPersistentStoreCoordinator *coord = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: model];
+    [coord addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:nil options:nil error:nil];
+    _context = [[NSManagedObjectContext alloc] init];
+    [_context setPersistentStoreCoordinator: coord];
 }
 
 - (void)tearDown
 {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    //TODO: delete all data
     [super tearDown];
 }
 
-/*
-- (void)testExample
+- (void)testInsertingJobs
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}*/
+    NSError *error;
+    NSFetchRequest *allJobs = [[NSFetchRequest alloc] init];
+    [allJobs setEntity:[NSEntityDescription entityForName:@"Job" inManagedObjectContext:_context]];
+    [allJobs setIncludesPropertyValues:NO]; //only fetch the managedObjectID
+    NSArray *origjobs = [_context executeFetchRequest:allJobs error:&error];
 
+    [NSEntityDescription insertNewObjectForEntityForName:@"Job" inManagedObjectContext:_context];
+    
+    NSArray *newjobs = [_context executeFetchRequest:allJobs error:&error];
+    
+    XCTAssert(newjobs.count==origjobs.count+1, @"jobs count should incrase by 1 to %d, instead got %d",origjobs.count+1,newjobs.count);
+}
+
+- (void)testInsertingViews
+{
+    
+}
+
+- (void)testInsertingJenkinsInstances
+{
+    
+}
+
+- (void)testInsertingBuilds
+{
+    
+}
+
+/*
 - (void)testImportAllJenkinsViews
 {
     KDBJenkinsRequestHandler *requestHandler = [[KDBJenkinsRequestHandler alloc] init];
@@ -45,6 +80,6 @@
     KDBJenkinsRequestHandler *requestHandler = [[KDBJenkinsRequestHandler alloc] init];
     [requestHandler importAllJobs];
     XCTFail(@"Verify some shit dude");
-}
+}*/
 
 @end
