e29cc4ab7042ff83dfac6e5f870923e74fa558ac
Kyle Beal <kyle.beal@thermofisher.com>
2014-05-09 19:29:25 -0400

    started to implement job detail in storyboard

---
+++ JenkinsMobile/KDBBuildsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KDBBuildsTableViewController.h
+//  JenkinsMobile
+//
+//  Created by Kyle Beal on 5/8/14.
+//  Copyright (c) 2014 Kyle Beal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KDBBuildsTableViewController : UITableViewController
+
+@end
