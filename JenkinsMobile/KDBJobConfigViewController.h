f2008a961532a5bf2b91be57a4a8ea5727703628
Kyle Beal <kyle.beal@thermofisher.com>
2014-07-23 22:20:46 -0400

    initial job config view mockup

---
+++ JenkinsMobile/KDBJobConfigViewController.h
@@ -0,0 +1,13 @@
+//
+//  KDBJobConfigViewController.h
+//  JenkinsMobile
+//
+//  Created by Kyle Beal on 7/23/14.
+//  Copyright (c) 2014 Kyle Beal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KDBJobConfigViewController : UIViewController
+
+@end
