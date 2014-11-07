722b2fec667d95899891d7a451ea3f48cdfe90df
kbeal <kyledbeal@gmail.com>
2014-11-07 18:15:26 -0500

    corrected issue of creating initial jenkinsinstance on wrong context. added saveMainContext method to AppDelegate

--- JenkinsMobile/KDBAppDelegate.m
+++ JenkinsMobile/KDBAppDelegate.m
@@ -70,10 +70,9 @@
     
     JenkinsInstance *jinstance = [JenkinsInstance fetchJenkinsInstanceWithURL:jenkinsURL fromManagedObjectContext:self.mainMOC];
     if (jinstance == nil) {
-        jinstance = [JenkinsInstance createJenkinsInstanceWithValues:jenkins inManagedObjectContext:self.masterMOC];
+        jinstance = [JenkinsInstance createJenkinsInstanceWithValues:jenkins inManagedObjectContext:self.mainMOC];
+        [self saveMainContext];
     }
-    
-    [self saveContext];
     
     return jinstance;
 }
@@ -144,6 +143,21 @@
                 abort();
             }
         }];
+    }
+}
+
+- (void)saveMainContext
+{
+    NSManagedObjectContext *managedObjectContext = self.mainMOC;
+    if (managedObjectContext != nil) {
+        NSError *error = nil;
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+        [self saveContext];
     }
 }
 
