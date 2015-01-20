56666556441aa9f49af8bb8970c7cac9e45d4adf
kbeal <kyledbeal@gmail.com>
2015-01-20 18:58:50 -0500

    Created ActiveConfiguration NSManagedObject

--- JenkinsMobile/ActiveConfiguration.h
+++ JenkinsMobile/ActiveConfiguration.h
@@ -2,20 +2,56 @@
 //  ActiveConfiguration.h
 //  JenkinsMobile
 //
-//  Created by Kyle on 8/29/14.
-//  Copyright (c) 2014 Kyle Beal. All rights reserved.
+//  Created by Kyle on 1/20/15.
+//  Copyright (c) 2015 Kyle Beal. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "Job.h"
+#import <CoreData/CoreData.h>
 
-@interface ActiveConfiguration : NSObject
+@class Build, Job;
 
-@property (nonatomic, retain) NSString * name;
-@property (nonatomic, retain) NSString * url;
+@interface ActiveConfiguration : NSManagedObject
+
+@property (nonatomic, retain) id actions;
+@property (nonatomic, retain) NSString * activeConfiguration_description;
+@property (nonatomic, retain) id activeConfigurations;
+@property (nonatomic, retain) NSNumber * buildable;
 @property (nonatomic, retain) NSString * color;
+@property (nonatomic, retain) NSNumber * concurrentBuild;
+@property (nonatomic, retain) NSString * displayName;
+@property (nonatomic, retain) NSString * displayNameOrNull;
+@property (nonatomic, retain) id downstreamProjects;
+@property (nonatomic, retain) NSNumber * firstBuild;
+@property (nonatomic, retain) id healthReport;
+@property (nonatomic, retain) NSNumber * inQueue;
+@property (nonatomic, retain) NSNumber * keepDependencies;
+@property (nonatomic, retain) NSNumber * lastBuild;
+@property (nonatomic, retain) NSNumber * lastCompletedBuild;
+@property (nonatomic, retain) NSNumber * lastFailedBuild;
+@property (nonatomic, retain) NSNumber * lastImportedBuild;
+@property (nonatomic, retain) NSNumber * lastStableBuild;
+@property (nonatomic, retain) NSNumber * lastSuccessfulBuild;
+@property (nonatomic, retain) NSDate * lastSync;
+@property (nonatomic, retain) NSNumber * lastUnstableBuild;
+@property (nonatomic, retain) NSNumber * lastUnsuccessfulBuild;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * nextBuildNumber;
+@property (nonatomic, retain) id property;
+@property (nonatomic, retain) id queueItem;
+@property (nonatomic, retain) id scm;
+@property (nonatomic, retain) NSData * testResultsImage;
+@property (nonatomic, retain) id upstreamProjects;
+@property (nonatomic, retain) NSString * url;
+@property (nonatomic, retain) NSSet *rel_ActiveConfiguration_Builds;
+@property (nonatomic, retain) Job *rel_ActiveConfiguration_Job;
+@end
 
-- (id) initWithName: (NSString *)name Color: (NSString *)color andURL: (NSString *)url;
-- (BOOL) isBuilding;
+@interface ActiveConfiguration (CoreDataGeneratedAccessors)
+
+- (void)addRel_ActiveConfiguration_BuildsObject:(Build *)value;
+- (void)removeRel_ActiveConfiguration_BuildsObject:(Build *)value;
+- (void)addRel_ActiveConfiguration_Builds:(NSSet *)values;
+- (void)removeRel_ActiveConfiguration_Builds:(NSSet *)values;
 
 @end
--- JenkinsMobile/ActiveConfiguration.m
+++ JenkinsMobile/ActiveConfiguration.m
@@ -2,23 +2,48 @@
 //  ActiveConfiguration.m
 //  JenkinsMobile
 //
-//  Created by Kyle on 8/29/14.
-//  Copyright (c) 2014 Kyle Beal. All rights reserved.
+//  Created by Kyle on 1/20/15.
+//  Copyright (c) 2015 Kyle Beal. All rights reserved.
 //
 
 #import "ActiveConfiguration.h"
-#import "Constants.h"
+#import "Build.h"
+#import "Job.h"
+
 
 @implementation ActiveConfiguration
 
-- (id) initWithName: (NSString *)name Color: (NSString *)color andURL: (NSString *)url
-{
-    self.name=name;
-    self.color=color;
-    self.url=url;
-    return self;
-}
-
-- (BOOL) isBuilding { return [self.color rangeOfString:@"anime"].length > 0 ? true : false; }
+@dynamic actions;
+@dynamic activeConfiguration_description;
+@dynamic activeConfigurations;
+@dynamic buildable;
+@dynamic color;
+@dynamic concurrentBuild;
+@dynamic displayName;
+@dynamic displayNameOrNull;
+@dynamic downstreamProjects;
+@dynamic firstBuild;
+@dynamic healthReport;
+@dynamic inQueue;
+@dynamic keepDependencies;
+@dynamic lastBuild;
+@dynamic lastCompletedBuild;
+@dynamic lastFailedBuild;
+@dynamic lastImportedBuild;
+@dynamic lastStableBuild;
+@dynamic lastSuccessfulBuild;
+@dynamic lastSync;
+@dynamic lastUnstableBuild;
+@dynamic lastUnsuccessfulBuild;
+@dynamic name;
+@dynamic nextBuildNumber;
+@dynamic property;
+@dynamic queueItem;
+@dynamic scm;
+@dynamic testResultsImage;
+@dynamic upstreamProjects;
+@dynamic url;
+@dynamic rel_ActiveConfiguration_Builds;
+@dynamic rel_ActiveConfiguration_Job;
 
 @end
