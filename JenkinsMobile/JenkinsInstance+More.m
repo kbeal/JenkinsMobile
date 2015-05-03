6928f98cc75915f4da6b6c77336711e053c6f199
kbeal <kyledbeal@gmail.com>
2015-05-03 13:04:21 -0400

    Fixed bug where primaryView was being inserted on every sync.

--- JenkinsMobile/JenkinsInstance+More.m
+++ JenkinsMobile/JenkinsInstance+More.m
@@ -141,15 +141,14 @@
     }
     
     for (NSDictionary *view in viewValues) {
-        if (![currentViewURLs containsObject:[view objectForKey:ViewURLKey]]) {
-            NSMutableDictionary *mutview = [NSMutableDictionary dictionaryWithDictionary:view];
-            [mutview setObject:self forKey:ViewJenkinsInstanceKey];
-
-            if ([mutview objectForKey:ViewNameKey] == [self.primaryView objectForKey:ViewNameKey]) {
-                NSString *fullViewURL = [NSString stringWithFormat:@"%@%@%@%@",[mutview objectForKey:ViewURLKey],@"view/",[mutview objectForKey:ViewNameKey],@"/"];
-                [mutview setObject:fullViewURL forKey:ViewURLKey];
-            }
-            
+        NSMutableDictionary *mutview = [NSMutableDictionary dictionaryWithDictionary:view];
+        [mutview setObject:self forKey:ViewJenkinsInstanceKey];
+        
+        if ([mutview objectForKey:ViewNameKey] == [self.primaryView objectForKey:ViewNameKey]) {
+            NSString *fullViewURL = [NSString stringWithFormat:@"%@%@%@%@",[mutview objectForKey:ViewURLKey],@"view/",[mutview objectForKey:ViewNameKey],@"/"];
+            [mutview setObject:fullViewURL forKey:ViewURLKey];
+        }
+        if (![currentViewURLs containsObject:[mutview objectForKey:ViewURLKey]]) {
             View *newView = [View createViewWithValues:mutview inManagedObjectContext:self.managedObjectContext];
             [currentViews addObject:newView];
         }
